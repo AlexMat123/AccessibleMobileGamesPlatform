8839a5baf83ecf4088fb7a638ce4b9acfc80e27b
Alexandru Mataoanu <mataoanua@cardiff.ac.uk>
2025-11-14 13:32:09 +0000

    resolving merge conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar.jsx';
 import Game from "./pages/Game.jsx";
 
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
