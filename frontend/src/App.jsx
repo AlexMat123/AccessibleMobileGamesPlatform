6f4cb84ee7268376179bbc7c2d99a824f69d0f16
c22058031 <feredayt@cardiff.ac.uk>
2025-11-18 09:59:19 +0000

    commiting stuff to allow me to have something to go back to while i fix a problem on my end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Search from "./pages/Search";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Game from "./pages/Game.jsx";
+import Library from "./pages/Library.jsx";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
         <Route path="/games/:id" element={<Game/>} />
+        <Route path="/Library" element={<Library/>} />
       </Routes>
     </Router>
   );
